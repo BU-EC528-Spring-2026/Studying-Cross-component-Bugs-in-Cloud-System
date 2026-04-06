47d9834c49f2aaeaf750c71051c6d3ccfc5b286e
Yiyang Hong <samuhong@bu.edu>
2026-04-06 01:33:44 -0400

    Add files via upload

---
+++ prototype/examples/spark4_dual_jdk_buggy/spark-env.sh
@@ -0,0 +1,2 @@
+export JAVA_HOME=/opt/openjdk-17
+export HADOOP_CONF_DIR=/etc/hadoop/conf
---
+++ prototype/examples/spark4_dual_jdk_buggy/submit.cmd
@@ -0,0 +1,1 @@
+./bin/spark-submit --master yarn --deploy-mode cluster --conf spark.app.name=Spark4DualJdkBuggy app.py
